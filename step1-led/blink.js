d494de526ef80d55824187023e826d5b47900657
gecko25 <sara.tankard@gmail.com>
2015-11-22 19:18:38 -0500

    step1 & step2

---
+++ step1-led/blink.js
@@ -0,0 +1,30 @@
+var five = require("johnny-five");
+var myBoard = new five.Board();
+
+/*Define a pin object*/
+var redLED = new five.Led(11);
+var greenLED = new five.Led(12);
+var yellowLED = new five.Led(13);
+
+
+myBoard.on("ready", function() {
+
+
+
+    /*Potientiometer*/
+    var pmeter = new five.Sensor("A0");
+
+    pmeter.on("change", function() {
+        // this.value will reflect a scaling from 0-1023 to 0-180
+        console.log( this.value );
+        greenLED.on();
+    });
+});
+
+
+/*Press a green button in UI to toggle button, turn it off, or turn it on*/
+/*Also create random events that will change the color of the LED
+ and have our program track these changes.
+ *To do this with express
+ */
+
---
+++ step1-led/cheerful.js
@@ -0,0 +1,52 @@
+/*
+ * Modified from: https://github.com/Resseguie/Cheerful-J5/blob/master/cheerful.js
+ */
+
+var request = require("request"),
+    five = require("johnny-five"),
+    Spark = require("spark-io"),
+    colorMap = require("./cheerlights-colors"),
+    board,
+    pins;
+
+board.on("ready", function() {
+
+    var lastColor = "white",
+        led = new five.Led.RGB(pins);
+
+    this.repl.inject({
+        led: led
+    });
+
+    led.color( colorMap[lastColor] );
+
+    setInterval(function() {
+        getLatestColor(function(err, color) {
+            if ( !err && colorMap[color] ) {
+                if ( color != lastColor ) {
+                    lastColor = color;
+                    console.log( "Changing to " + color );
+                    led.color( colorMap[color] );
+                }
+            }
+        });
+    }, 3000);
+
+});
+
+function getLatestColor(callback) {
+    request(
+        {
+            url: "https://api.thingspeak.com/channels/1417/feed/last.json",
+            json: true
+        }, function (error, response, body) {
+
+            if ( !error && response.statusCode === 200 ) {
+                var color = body.field1;
+                callback(null, color);
+            } else {
+                callback(error, null);
+            }
+        }
+    );
+}
